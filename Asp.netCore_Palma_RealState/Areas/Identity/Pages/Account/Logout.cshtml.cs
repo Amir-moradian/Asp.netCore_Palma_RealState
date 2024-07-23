fec5b7ffe6b79df0d942a01ba978629bf1e46d16
amir.moradian011@gmail.com <amir.moradian011@gmail.com>
2024-07-23 13:29:39 +0330

    login

--- Asp.netCore_Palma_RealState/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Asp.netCore_Palma_RealState/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using Asp.netCore_Palma_RealState.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,19 +15,19 @@ namespace Asp.netCore_Palma_RealState.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+        private readonly SignInManager<User_Model> _signInManager;
+       
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User_Model> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
-            _logger = logger;
+            
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
+           
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
---
+++ Asp.netCore_Palma_RealState/Models/ViewModel/login_VWM.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Asp.netCore_Palma_RealState.Models.ViewModel
+{
+    public class login_VWM
+    {
+        [Required]
+       public string phone { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+        public string? ReturnUrl { get; set; }
+
+        [TempData]
+        public string? ErrorMessage { get; set; }
+
+    }
+}
